ea5d8c899a29edb82019ba8aff341762c3360b5c
Carckv2 <119148632+Carckv2@users.noreply.github.com>
2023-02-18 17:14:24 +0000

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,4 @@
 # vendor tree
 git clone https://github.com/goldfish07/android_vendor_samsung_j8y18lte.git -b 11 vendor/samsung/j8y18lte
 
+git clone https://github.com/Carckv2/j8y18lte.git device/samsung/j8y18lte
